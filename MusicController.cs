debc1c844afbf9b660e283b824e4d86e16913c41
Jozsef Bali <70275083+Fenrir-Codes@users.noreply.github.com>
2022-09-19 11:15:45 +0200

    Add files via upload

--- MusicController.cs
+++ MusicController.cs
@@ -12,6 +12,10 @@ public class MusicController : MonoBehaviour
     public AudioClip BFGforLMG;
     public AudioClip BFGforMinigun;
 
+    private int LMG = 2;
+    private int Minigun = 4;
+    private int i;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,39 +27,78 @@ public class MusicController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        int i = 0;
-        if (!pauseMenu.isPaused && !musicPlayer.isPlaying)
+        PlayMusic();
+    }
+
+    #region play the music
+    private void PlayMusic()
+    {
+        if (pauseMenu.isPaused)
         {
-            i++;
-            var music = Random.Range(0, audioClips.Length);
-            musicPlayer.clip = audioClips[music];
-            musicPlayer.Play();
+            musicPlayer.Pause();
+        }
+        else
+        {
+            musicPlayer.UnPause();
+            i = 0;
 
-            if (i >= audioClips.Length)
+            if (inputController.activeWeapon != LMG | inputController.activeWeapon != Minigun)
             {
-                musicPlayer.clip = audioClips[i];
-                musicPlayer.Play();
+                PlayRandomFromList();
             }
-
-            if (pauseMenu.isPaused)
+            if (inputController.activeWeapon == LMG)
             {
-                musicPlayer.Pause();
+                PlayLMGClip();
             }
-            else
+            if (inputController.activeWeapon == Minigun)
             {
-                musicPlayer.UnPause();
+                PlayMinigunClip();
             }
-
-            //if (inputController.activeWeapon == 4)
-            //{
-            //    musicPlayer.Stop();
-            //    musicPlayer.clip = BFGforMinigun;
-            //    musicPlayer.Play();
-            //}
 
         }
-
-
-
     }
+    #endregion
+
+    #region play random from array
+    private void PlayRandomFromList()
+    {
+        i++;
+        if (!musicPlayer.isPlaying)
+        {
+            var music = Random.Range(i, audioClips.Length);
+            musicPlayer.clip = audioClips[music];
+            musicPlayer.volume = 0.3f;
+            musicPlayer.Play();
+        }
+        if (i >= audioClips.Length)
+        {
+            musicPlayer.clip = audioClips[i];
+            musicPlayer.Play();
+        }
+    }
+    #endregion
+
+    #region Play big fucking gun clip for LMG
+    private void PlayLMGClip()
+    {
+        musicPlayer.clip = BFGforLMG;
+        musicPlayer.volume = 1f;
+        if (!musicPlayer.isPlaying)
+        {
+            musicPlayer.Play();
+        }
+    }
+    #endregion
+
+    #region Play big fucking gun clip for Minigun
+    private void PlayMinigunClip()
+    {
+        musicPlayer.clip = BFGforMinigun;
+        musicPlayer.volume = 1f;
+        if (!musicPlayer.isPlaying)
+        {
+             musicPlayer.Play();
+        }
+    }
+    #endregion
 }
